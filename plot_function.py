fb877a5b29d01207e2fc3859c2c0cea19dd791e1
Otto Kuparsaari <otto.kuparsaari@gmail.com>
2021-07-19 23:37:14 +0300

    yes

--- plot_function.py
+++ plot_function.py
@@ -4,29 +4,23 @@ from matplotlib.animation import FuncAnimation
 
 
 fig = plt.figure()
-t = np.arange(0,50,0.05)
+t = np.arange(0,50,0.01)
 
-def lissajous(t=t,n=1.5,a=5,b=5,c=0):
-    x = []
-    y = []
-    
+def lissajous(i,n=1.5,a=5,b=5,c=0):
+
     def lissajous_x(t,n=n,a=a,c=c):
         return a*np.sin(n*t+c)
 
     def lissajous_y(t,b=b):
         return b*np.sin(t)
-    
-    for i in t:
-        x.append(lissajous_x(i))
-        y.append(lissajous_y(i))
-    
-    return np.array((x,y))
+
+    yield lissajous_x(i),lissajous_y(i)
 
 def update(rotation):
     plt.clf()
-    frame = a = lissajous(n=1+4/6,c=rotation/20)
-    plt.plot(a[1], a[0])
+    out = list(lissajous(t,c=rotation*10))[0]
+    plt.plot(out[1],out[0])
 
-anim = FuncAnimation(fig,update)
-#plt.show()
+anim = FuncAnimation(fig,update,frames=t)
 #anim.save("lissajous_anim_n=1,666.gif",fps=120)
+plt.show()
